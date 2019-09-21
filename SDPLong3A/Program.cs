e676a5e5a33f3bc9dece1519b7e12cbcec50c279
Long Le <olole005@edu.xamk.fi>
2019-09-21 17:17:50 +0300

    first commit

---
+++ SDPLong3A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SDPLong3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the SDPProject3A");
+
+            // Changes locally
+            Console.WriteLine("This is the change that I have made locally");
+        }
+    }
+}
